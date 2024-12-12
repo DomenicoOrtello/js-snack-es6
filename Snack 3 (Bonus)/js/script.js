3b88f9abcd7bf18d151ec3889f65a54a4436ce55
Ortello7 <domenicoortello00@gmail.com>
2024-12-12 16:12:07 +0100

    feat snack 3 bonus

--- Snack 3 (Bonus)/js/script.js
+++ Snack 3 (Bonus)/js/script.js
@@ -1,2 +1,17 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+// Funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+function filterArray(array, a, b) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (a <= i && i <= b) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
+
+// Risultato in console
+const resultArray = [10, 20, 30, 40, 50, 60];
+console.log(filterArray(resultArray, 1, 4));
